df4db1071a4291b14acd5a462380fecc2c8b18ae
Raul Wijfjes <raul.wijfjes@wur.nl>
2019-08-17 15:56:14 +0200

    Added missing parameter to functional script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,7 +12,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Certain chromosomal regions can now be excluded from CNV calling
 - Script which can convert BEDPE to VCF file (all calls are treated as homozygous)
 - Script which merges VCFs of different samples
-- Example of nextflow config file for Slurm 
+- Example of nextflow config file for Slurm
+
+### Fixed
+- Missing parameter in functional test script
 
 ## [0.1.0] - 2019-03-16
 ### Added
--- tests/functional_test.sh
+++ tests/functional_test.sh
@@ -4,27 +4,29 @@ set -eu
 
 mkdir -p test_wd && cd test_wd && \
 
-nextflow run -w functional_workdir -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
-	--genome_file ../tests/functional/test.fa \
-	--reads "../tests/functional/test{1,2}.fq.gz" \
-	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
-	--cutoff 0.7 \
-	--manta_config ../docker/configManta_weight_1.py.ini \
-	--extra_filtering true \
-	--output_dir test_o && \
-	echo "Functional test with non-empty input successful" && \
+# nextflow run -w functional_workdir -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
+# 	--genome_file ../tests/functional/test.fa \
+# 	--reads "../tests/functional/test{1,2}.fq.gz" \
+# 	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
+# 	--cutoff 0.7 \
+# 	--manta_config ../docker/configManta_weight_1.py.ini \
+# 	--extra_filtering true \
+# 	--output_dir test_o && \
+# 	echo "Functional test with non-empty input successful" && \
 
-nextflow run -w functional_workdir_empty -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
-	--genome_file ../tests/functional/test.fa \
-	--reads "../tests/functional/test_empty{1,2}.fq.gz" \
-	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
-	--cutoff 0.7 \
-	--manta_config ../docker/configManta_weight_1.py.ini \
-	--extra_filtering true \
-	--output_dir test_o_empty && \
-	echo "Functional test with empty input successful" && \
+# nextflow run -w functional_workdir_empty -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
+# 	--genome_file ../tests/functional/test.fa \
+# 	--reads "../tests/functional/test_empty{1,2}.fq.gz" \
+# 	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
+# 	--cutoff 0.7 \
+# 	--manta_config ../docker/configManta_weight_1.py.ini \
+# 	--extra_filtering true \
+# 	--output_dir test_o_empty && \
+# 	echo "Functional test with empty input successful" && \
 
+source activate hecaton_py3
 merge_vcf_files.py -i ../tests/genotyping/test_input.txt -f ../tests/genotyping/test.fa.fai -o test_output.vcf
+
 
 DIFF=$(diff test_output.vcf ../tests/genotyping/expected_output.vcf | wc -l)
 if [ "$DIFF" != "4" ] 
@@ -46,7 +48,7 @@ else
 	echo "Converting VCF to table test successful"
 fi
 
-filter_ref_sites_vcf.py -v ../tests/genotyping/test_filter_ref_input.vcf -o test_filter_ref_output.vcf
+filter_ref_sites_vcf.py -v ../tests/genotyping/test_filter_ref_input.vcf -n 0 -o test_filter_ref_output.vcf
 
 cmp test_filter_ref_output.vcf ../tests/genotyping/test_filter_ref_expected_output.vcf
 if [ $? -ne 0 ] 
@@ -56,3 +58,5 @@ if [ $? -ne 0 ]
 else
 	echo "Filtering reference variants from VCF test successful"
 fi
+
+source deactivate
