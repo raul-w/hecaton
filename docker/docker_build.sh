4274d571242f7472cf66b895d134279281acb576
Raul Wijfjes <raul.wijfjes@wur.nl>
2019-12-16 16:24:03 +0100

    Updated Docker image version number

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker build -t hecaton:v0.2.2 .
+docker build -t hecaton:v0.3.0 .
--- tests/functional_test_docker.sh
+++ tests/functional_test_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p test_wd_docker && cd test_wd_docker && \
 
-nextflow run -w functional_workdir -with-docker hecaton:v0.2.2 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
+nextflow run -w functional_workdir -with-docker hecaton:v0.3.0 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
 	--genome_file ../tests/functional/test.fa \
 	--reads "../tests/functional/test{1,2}.fq.gz" \
 	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
@@ -14,7 +14,7 @@ nextflow run -w functional_workdir -with-docker hecaton:v0.2.2 -c ../nextflow/ne
 	--output_dir test_o && \
 	echo "Functional test with non-empty input successful" && \
 
-nextflow run -w functional_workdir_empty -with-docker hecaton:v0.2.2 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
+nextflow run -w functional_workdir_empty -with-docker hecaton:v0.3.0 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton.nf \
 	--genome_file ../tests/functional/test.fa \
 	--reads "../tests/functional/test_empty{1,2}.fq.gz" \
 	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
@@ -24,7 +24,7 @@ nextflow run -w functional_workdir_empty -with-docker hecaton:v0.2.2 -c ../nextf
 	--output_dir test_o_empty && \
 	echo "Functional test with empty input successful" && \
 
-nextflow run -w functional_workdir_no_align -with-docker hecaton:v0.2.2 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton_no_align.nf \
+nextflow run -w functional_workdir_no_align -with-docker hecaton:v0.3.0 -c ../nextflow/nextflow.config -resume ../nextflow/hecaton_no_align.nf \
 	--genome_file ../tests/functional/test.fa \
 	--bwa_bams "../tests/functional/test.bam" \
 	--model_file ../models/random_forest_model_concat_A_thaliana_ColxCvi_O_sativa_Suijing18_coverage_10x_insertions_balanced_subsample.pkl \
